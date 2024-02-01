5f8f6c85894cda0ce366d1e9d7dcff1d0374f1fc
ovsyanur <141704103+ovsyanur@users.noreply.github.com>
2024-02-01 22:46:09 +0300

    second iteration

--- scripts/cards.js
+++ scripts/cards.js
@@ -2,25 +2,31 @@ const initialCards = [
     {
       name: "Архыз",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+      description: "Горная местность, покрытая травой и снегом",
     },
     {
       name: "Челябинская область",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+      description: "Зимняя река, берега которой покрыты снегом и лесом",
     },
     {
       name: "Иваново",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+      description: "Пейзаж из панельных домов, уходящий вдаль",
     },
     {
       name: "Камчатка",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+      description: "Травы и маленькие растения в горах",
     },
     {
       name: "Холмогорский район",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+      description: "Железная дорога, пролегающая через зелёный лес",
     },
     {
       name: "Байкал",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+      description: "Горный берег снежного озера свысока",
     }
 ];
